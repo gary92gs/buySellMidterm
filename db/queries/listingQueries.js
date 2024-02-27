0c260f3d5bcc17dc21f21007135ce5c4a6d510e7
gary92gs <gary92.gs@gmail.com>
2024-02-27 09:38:11 -0700

    add skeleton for future queries

---
+++ db/queries/listingQueries.js
@@ -0,0 +1,17 @@
+//LISTINGS
+const createListing = (listingObj) => {
+  //inserts a new entry in the db's listing table
+}
+
+const getListing = (listingId) =>{
+  //retrieves individual listing
+}
+
+const updateListing = (listingId,updatedFieldsObj) => {
+  //receives object with fields that need to be updated
+}
+
+const deleteListing = (listingId) => {
+  //search db listings table by listing_id
+  //update status to False
+}
---
+++ db/queries/userQueries.js
@@ -0,0 +1,18 @@
+
+
+const getFeaturedListings = () =>{
+  //how do we determine what is featured? newest? Should I just grab the newest item from each category?
+  //
+}
+
+//USERS
+const isValidUserCredentials = (userCredentialsObj)=>{
+  //check database for user by email or username
+  //verify that password is correct
+}
+
+const registerUser = (userObj) =>{
+  //using data from registration process
+  //verify user email or username has not been used before
+    //then insert new user into database
+}
