2f159e2d8c58b04b1d014878fae1496062c09a2c
Shadrak Lagadi <shadrl19@gmail.com>
2024-02-28 20:51:36 -0700

    Add db query for when db goes live. Still need to test for bugs

--- routes/show.js
+++ routes/show.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const router = express.Router();
 
+// uncomment when db is live
+// const db = require('../connection');
+
 const listingsData = [
   { id: 1, title: 'Listing 1', description: 'Description for listing 1' },
   { id: 2, title: 'Listing 2', description: 'Description for listing 2' },
@@ -25,5 +28,25 @@ router.get('/:id', (req, res) => {
   }
   res.json(uniqueListing);
 });
+
+// test when db goes live
+
+// router.get('/:id', (req, res) => {
+//   const requestedId = parseInt(req.params.id);
+//   db.query('SELECT * FROM listings WHERE id = $1', [requestedId])
+//     .then((result) => {
+//       if (result.rows.length > 0) {
+//         const uniqueListing = result.rows[0];
+//         return res.render('tbd', { listing: uniqueListing });
+//       }
+//       console.log('Listing not found');
+//       return res.status(404).send("Please try again")
+//     })
+//     .catch((error) => {
+//       console.error('Error fetching listing:', error);
+//       return res.status(500).send("Internal Server Error")
+//     });
+// });
+
 
 module.exports = router;
