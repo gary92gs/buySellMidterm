8025f7bd633687e0a08701b59eabaac0a9ce476b
Shadrak Lagadi <shadrl19@gmail.com>
2024-02-28 21:27:07 -0700

    Add post request to post new listing to db. Redirect to show the new listing /show/:id

--- routes/new.js
+++ routes/new.js
@@ -1,19 +1,43 @@
 const express = require('express');
 const router  = express.Router();
 
+// uncomment when db is live
+// const db = require('../connection');
+
 router.get('/', (req, res) => {
   return res.status(200).send('You should be seeing this');
 })
 
 // router.get('/new', (req, res) => {
-//   userQueries.getUsers()
-//     .then(users => {
-//       res.json({ users });
+  
+//   res.render('tbd');
+// });
+
+// router.post('/new', (req, res) => {
+//   const {
+//     owner_id,
+//     title,
+//     description,
+//     price_cents,
+//     street_address,
+//     city,
+//     country,
+//     postal_code,
+//     status
+//   } = req.body;
+
+//   db.query(`
+//     INSERT INTO listings (owner_id, title, description, price_cents, street_address, city, country, postal_code, status)
+//     VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9)
+//     RETURNING id;`, 
+//     [owner_id, title, description, price_cents, street_address, city, country, postal_code, status])
+//     .then((result) => {
+//       const newListingId = result.rows[0].id;
+//       return res.redirect(`/show/${newListingId}`);
 //     })
-//     .catch(err => {
-//       res
-//         .status(500)
-//         .json({ error: err.message });
+//     .catch((error) => {
+//       console.error('Error adding new listing:', error);
+//       return res.status(500).send('Internal Server Error');
 //     });
 // });
 
