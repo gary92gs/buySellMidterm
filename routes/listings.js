7811197fe58ec50e5706c3b859d6e0310c53d6d2
Shadrak Lagadi <shadrl19@gmail.com>
2024-03-01 11:11:27 -0700

    Linted

--- routes/listings.js
+++ routes/listings.js
@@ -3,7 +3,7 @@ const router = express.Router();
 // uncomment when db connected
 // const db = require('../connection');
 
-const listingsData = [
+let listings = [
   { id: 1, title: 'Listing 1', description: 'Description for listing 1' },
   { id: 2, title: 'Listing 2', description: 'Description for listing 2' },
   { id: 3, title: 'Listing 3', description: 'Description for listing 3' }
@@ -11,7 +11,7 @@ const listingsData = [
 
 router.get('/', (req, res) => {
   // Send JSON for listingsData
-  res.json(listingsData);
+  res.json(listings);
 });
 
 // test when connected
@@ -19,14 +19,14 @@ router.get('/', (req, res) => {
 // router.get('/'), (req, res) => {
 //   db.query(`SELECT *
 //   FROM listings`)
-//   .then((results) => {
-//     listingsData = results.rows;
-//     return res.render('listings', { listingsData });
-//   })
-//   .catch((error) => {
-//     console.log(error) 
-//     return res.status(500).send("Internal Server Error")
-//   })
-// }
+//     .then((results) => {
+//       listings = results.rows;
+//       return res.render('listings', { listings });
+//     })
+//     .catch((error) => {
+//       console.log(error);
+//       return res.status(500).send("Internal Server Error");
+//     });
+// };
 
 module.exports = router;
