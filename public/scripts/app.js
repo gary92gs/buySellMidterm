1ae18bc4974d19ffbcc41e40296cfcc1f0b89159
Shadrak Lagadi <shadrl19@gmail.com>
2024-03-06 12:16:04 -0700

    Create Ajax request for DELETE method

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -2,17 +2,24 @@
 
 $(document).ready(function() {
 
-  $('#post-ad-btn').on('submit', function(e) {
-    e.preventDefault();//prevents form from being submitted 
-  });
+  $('.delete-button').click(function() {
 
+    const listingId = $(this).closest('tr').data('listing-id');
+    console.log('listing ID:', listingId)
+    $.ajax({
+        url: 'favourites/delete',
+        method: 'DELETE',
+        data: { listingId },
+        success: function(response) {
+            console.log('Listing deleted successfully');
+            $('#item-' + listingId).remove();
 
-
-
-
-
-
-
-
+            window.location.href = '/favourites';
+        },
+        error: function(xhr, status, error) {
+            console.error('Error deleting listing:', error);
+        }
+    });
+});
 
 });
