8f039cb2d63641e310a083770410f954fc39342a
Shadrak Lagadi <shadrl19@gmail.com>
2024-03-07 22:15:12 -0700

    Add team route

---
+++ routes/team.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const router = express.Router();
+
+router.get('/', (req, res) => {
+  // render the team page
+  const filterObj = {
+    priceCentsMin: Number(req.cookies.priceMin + '00'),
+    priceCentsMax: Number(req.cookies.priceMax + '00'),
+    userSearch: req.cookies.userSearch,
+    city: req.cookies.city,
+    province: req.cookies.province,
+    country: req.cookies.country,
+  };
+
+
+  res.render('team', { filterObj });
+});
+
+module.exports = router;
